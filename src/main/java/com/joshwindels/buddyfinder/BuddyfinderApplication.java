98de8ea9a4663620001ae2f755b95932962dfd5f
Josh.Windels <joshuawindels@gmail.com>
2018-04-02 01:05:54 +0100

    initial spring initializer files

---
+++ src/main/java/com/joshwindels/buddyfinder/BuddyfinderApplication.java
@@ -0,0 +1,12 @@
+package com.joshwindels.buddyfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyfinderApplication.class, args);
+	}
+}
