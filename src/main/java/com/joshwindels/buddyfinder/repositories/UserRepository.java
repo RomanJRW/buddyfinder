7a44576279a22f94068dabad7f47e544fedfefe3
Josh.Windels <joshuawindels@gmail.com>
2018-04-03 00:03:03 +0100

    add configured named parameter templates to repositories

--- src/main/java/com/joshwindels/buddyfinder/repositories/UserRepository.java
+++ src/main/java/com/joshwindels/buddyfinder/repositories/UserRepository.java
@@ -1,8 +1,13 @@
 package com.joshwindels.buddyfinder.repositories;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepository {
+
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
 }
