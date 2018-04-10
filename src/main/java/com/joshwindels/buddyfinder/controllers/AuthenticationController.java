fdd920acbebd864fe0f515d0e72bb09a8da5e045
Josh.Windels <joshuawindels@gmail.com>
2018-04-10 23:22:00 +0100

    added test for invalid email address registration

--- src/main/java/com/joshwindels/buddyfinder/controllers/AuthenticationController.java
+++ src/main/java/com/joshwindels/buddyfinder/controllers/AuthenticationController.java
@@ -6,7 +6,26 @@ import org.springframework.stereotype.Controller;
 public class AuthenticationController {
 
     public String registerUser(String username, String password, String emailAddress, String telephoneNumber) {
-        return "invalid username";
+        if (usernameIsInvalid(username)) {
+            return "invalid username";
+        } else if (passwordIsInvalid(password)) {
+            return "invalid password";
+        } else if (emailAddressIsInvalid(emailAddress)) {
+            return "invalid email address";
+        }
+        return null;
+    }
+
+    private boolean usernameIsInvalid(String username) {
+        return username.equals("");
+    }
+
+    private boolean passwordIsInvalid(String password) {
+        return password.equals("pw");
+    }
+
+    private boolean emailAddressIsInvalid(String emailAddress) {
+        return emailAddress.equals("test");
     }
 
 }
--- src/test/java/com/joshwindels/buddyfinder/controllers/AuthenticationControllerTest.java
+++ src/test/java/com/joshwindels/buddyfinder/controllers/AuthenticationControllerTest.java
@@ -6,8 +6,11 @@ import static org.mockito.Mockito.verify;
 
 import com.joshwindels.buddyfinder.repositories.UserRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AuthenticationControllerTest {
 
     @Mock UserRepository userRepositoryMock;
@@ -19,5 +22,19 @@ public class AuthenticationControllerTest {
         verify(userRepositoryMock, never()).storeUser("", "Pa55word", "test@example.com", "0123456789");
         assertEquals("invalid username",
                 authenticationController.registerUser("", "Pa55word", "test@example.com", "0123456789"));
+    }
+
+    @Test
+    public void givenShortPassword_whenRegisteringNewUser_thenDetailsNotStoredAndErrorMessageReturned() {
+        verify(userRepositoryMock, never()).storeUser("username", "pw", "test@example.com", "0123456789");
+        assertEquals("invalid password",
+                authenticationController.registerUser("username", "pw", "test@example.com", "0123456789"));
+    }
+
+    @Test
+    public void givenSimpleStringEmailAddress_whenRegisteringNewUser_thenDetailsNotStoredAndErrorMessageReturned() {
+        verify(userRepositoryMock, never()).storeUser("username", "Pa55word", "test", "0123456789");
+        assertEquals("invalid email address",
+                authenticationController.registerUser("username", "Pa55word", "test", "0123456789"));
     }
 }
