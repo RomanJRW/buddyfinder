0431d13799676725bcd8ee935a92a904403ad634
Josh.Windels <joshuawindels@gmail.com>
2018-04-12 02:42:50 +0100

    add initial repository method implementations

--- src/main/java/com/joshwindels/buddyfinder/repositories/UserRepository.java
+++ src/main/java/com/joshwindels/buddyfinder/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.joshwindels.buddyfinder.repositories;
 
 import com.joshwindels.buddyfinder.dos.UserDO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,20 +13,55 @@ public class UserRepository {
     NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public void storeUser(UserDO userDO) {
+        String sql = " INSERT INTO users (username, password, email_address, telephone_no) "
+                + "    VALUES (:username, :password, :emailAddress, :telephoneNumber) ";
 
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("username", userDO.getUsername());
+        params.addValue("password", userDO.getPassword());
+        params.addValue("emailAddress", userDO.getEmailAddress());
+        params.addValue("telephoneNumer", userDO.getTelephoneNumber());
+
+        namedParameterJdbcTemplate.update(sql, params);
     }
 
     public boolean userNameIsAvailable(String username) {
+        String sql = " SELECT exists(SELECT 1 FROM users WHERE username = :username)";
 
-        return true;
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("username", username);
+
+        return namedParameterJdbcTemplate.queryForObject(sql, params, Boolean.class);
     }
 
     public String getStoredPasswordForUser(String username) {
+        String sql = " SELECT password FROM users where username = :username ";
 
-        return null;
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("username", username);
+
+        return namedParameterJdbcTemplate.queryForObject(sql, params, String.class);
     }
 
     public void updateUser(UserDO userDO) {
+        String sql = "";
+        if (userDO.getEmailAddress() != null && userDO.getTelephoneNumber() != null) {
+            sql = "     UPDATE users SET email_address = :emailAddress "
+                    + " AND telephone_no = :telephoneNumber "
+                    + " WHERE username = :username ";
+        } else if (userDO.getEmailAddress() != null) {
+            sql = "     UPDATE users SET email_address = :emailAddress "
+                    + " WHERE username = :username ";
+        } else if (userDO.getTelephoneNumber() != null) {
+            sql = "     UPDATE users SET telephone_no = :telephoneNumber "
+                    + " WHERE username = :username ";
+        }
 
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("username", userDO.getUsername());
+        params.addValue("emailAddress", userDO.getEmailAddress());
+        params.addValue("telephoneNumer", userDO.getTelephoneNumber());
+
+        namedParameterJdbcTemplate.update(sql, params);
     }
 }
